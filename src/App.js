899d0e17fffb33bec080d9b1069ead023d93f3f4
Sergey Egorov <egorovsjob@gmail.com>
2020-09-06 16:41:47 +0700

    invite with bad pictures

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useCallback, useState } from "react";
+import css from "./App.module.scss";
+import logo from "./assets/logo.jpeg";
+import first from "./assets/first-page.jpeg";
+import second from "./assets/second-page.jpeg";
 
 function App() {
+  const [step, setStep] = useState(1);
+  const nextStep = useCallback(() => setStep((s) => s + 1), []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={css.container} onClick={nextStep}>
+      {step === 1 && <img className={css.image} src={logo} />}
+      {step === 2 && <img src={first} />}
+      {step >= 3 && <img src={second} />}
     </div>
   );
 }
