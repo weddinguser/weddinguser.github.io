c95ec4d9fe8f88bf3d005403fd586f2317b4a4ad
Sergey Egorov <egorovsjob@gmail.com>
2020-09-06 16:18:43 +0700

    Updates

---
+++ precache-manifest.57c232b64b61fb2e6849808465b8a73f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d19392f528c70f34ec843e39712638a7",
+    "url": "/weddinguser/weddinguser.github.io/index.html"
+  },
+  {
+    "revision": "bf034d0eb44a918da43b",
+    "url": "/weddinguser/weddinguser.github.io/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "051cd6d5fb3ae82afcd0",
+    "url": "/weddinguser/weddinguser.github.io/static/js/2.84efb5b5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weddinguser/weddinguser.github.io/static/js/2.84efb5b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf034d0eb44a918da43b",
+    "url": "/weddinguser/weddinguser.github.io/static/js/main.01f0fb1d.chunk.js"
+  },
+  {
+    "revision": "cdc131371181acea7926",
+    "url": "/weddinguser/weddinguser.github.io/static/js/runtime-main.0cd4c405.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/weddinguser/weddinguser.github.io/static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/weddinguser/weddinguser.github.io/precache-manifest.57c232b64b61fb2e6849808465b8a73f.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/weddinguser/weddinguser.github.io/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
